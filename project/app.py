f1b498aa89cb0d14b3af7f177283f38926236fd5
AhnSangRok <71484671+AhnSangRok@users.noreply.github.com>
2022-06-21 19:07:13 +0900

    식단 입력 페이지

--- project/app.py
+++ project/app.py
@@ -3,21 +3,58 @@ import requests
 from pymongo import MongoClient
 
 #pymongo
-client = MongoClient('mongodb+srv://test:sparta@cluster0.yqzgz.mongodb.net/?retryWrites=true&w=majority')
+# client = MongoClient('mongodb+srv://test:sparta@cluster0.yqzgz.mongodb.net/?retryWrites=true&w=majority')
+# db = client.dbsparta
+client = MongoClient('mongodb+srv://test:sparta@cluster0.fchsu.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 #flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
     return render_template("index.html")
 
-
 @app.route('/diet')
 def detail():
     return render_template("sub.html")
+
+@app.route('/diet/input')
+def input_page():
+    return render_template("sub_input_form.html")
+
+@app.route('/diet/input_menu', methods=["POST"])
+
+def input_menu():
+    day_receive = request.form['day_give']
+    time_receive = request.form['time_give']
+    main_receive = request.form['main_give']
+    mainkcal_receive = request.form['mainkcal_give']
+    img_receive = request.form['img_give']
+    sub_receive = request.form['sub_give']
+    subkcal_receive = request.form['subkcal_give']
+
+    doc = {
+        'day' : day_receive,
+        'time' : time_receive,
+        'main' : main_receive,
+        'mainkcal' : mainkcal_receive,
+        'img' : img_receive,
+        'sub' : sub_receive,
+        'subkcal' : subkcal_receive
+    }
+
+    users = list(db.prac.find({'day': day_receive}, {'_id': False}))
+
+    for user in users :
+        if user['time'] == doc['time']:
+            return jsonify({'msg': '작성한 이력이 있습니다.'})
+        else :
+            db.prac.insert_one(doc)
+            return jsonify({'msg': '입력완료'})
+
+    db.prac.insert_one(doc)
+    return jsonify({'msg': '입력완료'})
 
 
 if __name__ == '__main__':
