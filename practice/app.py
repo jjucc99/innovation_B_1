ef3ad43e1736fb69ba6ca2803b4c37578deece44
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-06-22 19:17:40 +0900

    최종 검토본

--- practice/app.py
+++ practice/app.py
@@ -1,19 +1,28 @@
-from flask import *
+import jwt
+import datetime
+import hashlib
 import requests
-from datetime import datetime
-from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from datetime import datetime, timedelta
+from werkzeug.utils import secure_filename
 
-#pymongo
-# client = MongoClient('mongodb+srv://test:sparta@cluster0.yqzgz.mongodb.net/?retryWrites=true&w=majority')
-# db = client.dbsparta
-client = MongoClient('mongodb+srv://test:sparta@cluster0.fchsu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://test:sparta@cluster0.qtj5c.mongodb.net/Cluster0?retryWrites=true&w=majority")
 db = client.dbsparta
 
-#flask
-app = Flask(__name__)
 
 @app.route('/')
-def main():
+def home():
+
+    token_receive = request.cookies.get('mytoken')
     year = datetime.today().year
     month = datetime.today().month
     week = []
@@ -43,7 +52,61 @@ def main():
             diet.append(list_menu)
         list_menu = None
 
-    return render_template("index.html", diet=diet, year=year, month=month)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template("index.html", diet=diet, year=year, month=month)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        # "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 @app.route('/diet/<day>')
 def detail(day):
@@ -52,10 +115,6 @@ def detail(day):
     month = datetime.today().month
     users = list(db.prac.find({'day': str(day)}, {'_id': False}))
     return render_template("sub.html", month=month, year=year, user=users)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
 
 @app.route('/diet/input')
 def input_page():
@@ -113,7 +172,6 @@ def get_comment(day,time):
     users = list(db.subcomment.find({'day':day,'time':time}, {'_id': False}))
     # print(day+time)
     return jsonify({'users':users})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
